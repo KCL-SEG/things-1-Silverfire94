9f5d56f29f49fb06deb013dd177f430472b901a3
Silverfire94 <markEmmanuel@yahoo.com>
2022-10-16 15:13:28 +0100

    as

--- things/models.py
+++ things/models.py
@@ -1,4 +1,4 @@
-from django.core.validators import RegexValidator, MaxLengthValidator, MinLengthValidator
+from django.core.validators import RegexValidator
 from django.db import models
 
 # Create your models here.
@@ -6,14 +6,3 @@ class Thing(models.Model):
     name = models.CharField(blank = False, max_length = 30, unique = True)
     description = models.CharField(max_length = 120, blank = True, unique = False)
     quantity = models.IntegerField()
-        #unique = False
-        #validators = [
-            #MaxLengthValidator(
-            #    limit_value = 100,
-            #    message = "The quantity can not be greater than 100"
-            #),
-            #MinLengthValidator(
-            #    limit_value = 0,
-            #    message = "The quantity can not be less than 0"
-            #)
-            #]
