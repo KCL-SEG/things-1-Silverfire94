7479e4ee94b7ea4de19ff0c89ea93ced38ce01ce
Silverfire94 <markEmmanuel@yahoo.com>
2022-10-16 15:28:32 +0100

    I have done the second excercise

--- things/models.py
+++ things/models.py
@@ -1,8 +1,8 @@
-from django.core.validators import RegexValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 # Create your models here.
 class Thing(models.Model):
     name = models.CharField(blank = False, max_length = 30, unique = True)
     description = models.CharField(max_length = 120, blank = True, unique = False)
-    quantity = models.IntegerField()
+    quantity = models.IntegerField(validators = [MinValueValidator(0), MaxValueValidator(100)])
