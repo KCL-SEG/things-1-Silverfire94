6660d6d382019be4ec0fbd789e7d94ac3da0f02f
Silverfire94 <markEmmanuel@yahoo.com>
2022-10-16 14:43:17 +0100

    I tried to do the second Excercise

--- things/models.py
+++ things/models.py
@@ -1,3 +1,8 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
 # Create your models here.
+class Thing(models.Model):
+    name = models.CharField( blank = False, max_length = 20)
+    description = models.CharField(max_length = 100, blank = False)
+    quantity = models.IntegerField()
