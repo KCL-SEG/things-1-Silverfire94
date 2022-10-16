42f6cfab1e400cc9af8f87f9906224ec9da5233b
Silverfire94 <markEmmanuel@yahoo.com>
2022-10-16 14:55:15 +0100

    I tried to do the second Excercise

--- things/models.py
+++ things/models.py
@@ -1,8 +1,19 @@
-from django.core.validators import RegexValidator
+from django.core.validators import RegexValidator, MaxLengthValidator, MinLengthValidator
 from django.db import models
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.CharField( blank = False, max_length = 20)
-    description = models.CharField(max_length = 100, blank = False)
-    quantity = models.IntegerField()
+    name = models.CharField( blank = False, max_length = 30, unique = True)
+    description = models.CharField(max_length = 120, blank = True, unique = False)
+    quantity = models.IntegerField(
+        unique = False,
+        validators = [
+            MaxLengthValidator(
+                limit_value = 100,
+                message = "The quantity can not be greater than 100"
+            ),
+            MinLengthValidator(
+                limit_value = 0,
+                message = "The quantity can not be less than 0"
+            )]
+    )
